5274970e214b543b3b523e29e0414c9bda2b9cd0
Glitch (christophermarek-inventory-application) <none>
2020-04-04 23:03:10 +0000

    Bare MVC layed down

---
+++ routes/item.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+// Require controller modules.
+var item_controller = require('../controllers/itemController');
+
+/// item ROUTES ///
+
+// GET item home page.
+router.get('/', item_controller.index);
+
+// GET request for creating a item.
+router.get('/item/create', item_controller.item_create_get);
+
+// POST request for creating item.
+router.post('/item/create', item_controller.item_create_post);
+
+// GET request to delete item.
+router.get('/item/:id/delete', item_controller.item_delete_get);
+
+// POST request to delete item.
+router.post('/item/:id/delete', item_controller.item_delete_post);
+
+// GET request to update item.
+router.get('/item/:id/update', item_controller.item_update_get);
+
+// POST request to update item.
+router.post('/item/:id/update', item_controller.item_update_post);
+
+// GET request for one item.
+router.get('/item/:id', item_controller.item_detail);
+
+// GET request for list of all item items.
+router.get('/item', item_controller.item_list);
+
+module.exports = router;
