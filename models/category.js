98aa48770452b82fd584d1d2fc9d151174fa093d
Glitch (christophermarek-inventory-application) <none>
2020-04-04 20:00:56 +0000

    Models and MongoDB setup

---
+++ models/category.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var CategorySchema = new Schema(
+  {
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+  }
+);
+
+// Virtual for book's URL
+CategorySchema
+.virtual('url')
+.get(function () {
+  return '/category/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Category', CategorySchema);
