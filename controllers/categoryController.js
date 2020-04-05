b358056091d8b031ceb1235ee841af98e284e586
Glitch (christophermarek-inventory-application) <none>
2020-04-05 01:54:25 +0000

    Added CRUD for Category Model

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,38 +1,56 @@
 var Category = require("../models/category");
 
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 //Send home page
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    res.render('index', { title: 'Express' });
 };
 
 // Display list of all Category.
 exports.category_list = function(req, res) {
-  res.send("NOT IMPLEMENTED: category list");
+  Category.find({}, function(err, categories) {
+      if(err) return console.error(err);
+      res.render('index', { title: 'Category List', category_list: categories });
+  });
 };
 
 // Display detail page for a specific Category.
 exports.category_detail = function(req, res) {
-  res.send("NOT IMPLEMENTED: category detail: " + req.params.id);
+  Category.findById(req.params.id, function(err, categoryData) {
+    if (err) return console.error(err);
+      res.render('category', {title: 'Category View', name: categoryData.name, description: categoryData.description, url: categoryData.url});
+  });
 };
 
 // Display Category create form on GET.
 exports.category_create_get = function(req, res) {
-  res.send("NOT IMPLEMENTED: category create GET");
+  res.render('category_form', { title: 'Create Category'});
 };
 
 // Handle Category create on POST.
 exports.category_create_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: category create POST");
+    var category = new Category({ name: req.body.name, description: req.body.description});
+    category.save(function (err) {
+      if (err) { return console.error(err); }
+      res.redirect('index');
+    });
+    
 };
 
 // Display Category delete form on GET.
-exports.category_delete_get = function(req, res) {
-  res.send("NOT IMPLEMENTED: category delete GET");
+exports.category_delete_get = function(req, res, next) {
+   
 };
 
 // Handle Category delete on POST.
-exports.category_delete_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: category delete POST");
+exports.category_delete_post = function(req, res, next) {
+  console.log(req.body.id);
+   Category.findByIdAndRemove(req.body.id, function deleteAuthor(err) {
+     if (err) { return next(err); }
+      res.redirect('/category');
+  })
 };
 
 // Display Category update form on GET.
